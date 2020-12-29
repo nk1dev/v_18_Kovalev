221090fadc97698b63e4d822c1cb0d2985f80925
nk1dev <76609654+nk1dev@users.noreply.github.com>
2020-12-30 00:12:44 +0300

    update

---
+++ app/src/main/java/com/example/v_18_kovalev/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.v_18_kovalev;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        findViewById(R.id.name).setVisibility(View.VISIBLE);
+    }
+}
